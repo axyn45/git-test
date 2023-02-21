ebd4f8b17868ab7e077d78f0f5f636e63a83e9df
axyn45 <axyn45@outlook.com>
2023-02-21 10:20:03 +0000

    03

--- autoACP.sh
+++ autoACP.sh
@@ -1,9 +1,14 @@
-#! /bin/ash
-echo "Git adding..."
+#! /bin/bash
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+PURPLE='\033[1;35m'
+NC='\033[0m'
+echo "${YELLOW}Git adding...${NC}"
 git add .
-echo "Git committing..."
-date_v=$date
-git commit -m "$date_v"
-echo "Git pushing..."
+echo "${PURPLE}Git committing...${NC}"
+export COMMIT_DATE=$(date)
+git commit -m "$COMMIT_DATE"
+echo "${CYAN}Git pushing...${NC}"
 git push origin master
-echo "Backup Done: $date_v"
+echo "${GREEN}Backup Done: $COMMIT_DATE${NC}"
